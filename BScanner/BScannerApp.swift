a4d2885dfe43655953bb8aee518a4a4b57bd4eb6
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-12-27 13:33:34 +0400

    Initial Commit

---
+++ BScanner/BScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BScannerApp.swift
+//  BScanner
+//
+//  Created by Hakob Ghlijyan on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
